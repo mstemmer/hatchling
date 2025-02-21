113fd3e745648afa836336693b3a271187dceffe
mstemmer <stemmer.manuel@gmail.com>
2025-02-21 20:25:11 +0000

    fan pwm working

--- hardware_test_scripts/fan_pwm_test.py
+++ hardware_test_scripts/fan_pwm_test.py
@@ -1,17 +1,27 @@
 from time import sleep
 from RPi import GPIO
+from rpi_hardware_pwm import HardwarePWM
 
-fanPin = 18
+fanPin = 19
 
-GPIO.setmode(GPIO.BCM)
-GPIO.setup(fanPin, GPIO.OUT)
-GPIO.output(fanPin, GPIO.LOW)
-
-
-fan = GPIO.PWM(fanPin, 25)
-fan.start(0)
+# GPIO.setmode(GPIO.BCM)
+# GPIO.setup(fanPin, GPIO.OUT)
+# GPIO.output(fanPin, GPIO.LOW)
 
 
-# fan.ChangeDutyCycle(50)
-sleep(6)
+# fan = GPIO.PWM(fanPin, 15000)
+# fan.start(0)
 
+
+# fan.ChangeDutyCycle(25)
+# sleep(6)
+
+
+pwm = HardwarePWM(pwm_channel=2, hz=25000, chip=2)
+pwm.start(100) # full duty cycle
+
+pwm.change_duty_cycle(70)
+sleep(9)
+# pwm.change_frequency(25_000)
+
+pwm.stop()
